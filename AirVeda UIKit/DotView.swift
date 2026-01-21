8089e7f532e4a6b66d90c6328352dc2d573796a6
patilvedant01 <vedantpatil324@gmail.com>
2026-01-21 23:34:20 +0530

    first commit

---
+++ AirVeda UIKit/DotView.swift
@@ -0,0 +1,87 @@
+//
+//  DotView.swift
+//  AirVeda UIKit
+//
+//  Created by Vedant Patil on 21/01/26.
+//
+
+import Foundation
+import UIKit
+
+class DotView: UIView {
+    
+    var dotDict: [Int: CGPoint] = [:]
+    var set = Set<CGPoint>()
+    var count = 0
+    var cannotFormNewLine: Bool = false
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpViews()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // If you want the view to always fill its superview when added without constraints:
+        if frame == .zero, let superview = superview {
+            frame = superview.bounds
+        }
+    }
+    
+    func setUpViews() {
+        backgroundColor = .green
+        isOpaque = true
+    }
+    
+    func configureDot(location: CGPoint) {
+        guard cannotFormNewLine == false else {
+            return
+        }
+        
+        // Optional: if you want to prevent new lines when tapping the exact same point
+        if set.contains(location) {
+            cannotFormNewLine = true
+        }
+        
+        let x = location.x
+        let y = location.y
+        
+        dotDict[count] = CGPoint(x: x, y: y)
+        set.insert(CGPoint(x: x, y: y))
+        
+        // Draw a small dot as a filled circle layer
+        let radius: CGFloat = 4.0
+        let dotRect = CGRect(x: x - radius, y: y - radius, width: radius * 2, height: radius * 2)
+        let dotPath = UIBezierPath(ovalIn: dotRect)
+        let dotLayer = CAShapeLayer()
+        dotLayer.path = dotPath.cgPath
+        dotLayer.strokeColor = UIColor.blue.cgColor
+        dotLayer.fillColor = UIColor.blue.cgColor
+        layer.addSublayer(dotLayer)
+        
+        // If there is a previous point, draw a line segment layer from last to current
+        if count > 0, let lastPoint = dotDict[count - 1] {
+            let linePath = UIBezierPath()
+            linePath.move(to: lastPoint)
+            linePath.addLine(to: CGPoint(x: x, y: y))
+            
+            let lineLayer = CAShapeLayer()
+            lineLayer.path = linePath.cgPath
+            lineLayer.strokeColor = UIColor.blue.cgColor
+            lineLayer.lineWidth = 2.0
+            lineLayer.fillColor = UIColor.clear.cgColor
+            layer.addSublayer(lineLayer)
+        }
+        
+        count += 1
+    }
+}
--- AirVeda UIKit/ViewController.swift
+++ AirVeda UIKit/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let dotView: DotView = DotView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpViews()
     }
-
-
+    
+    func setUpViews() {
+        view.backgroundColor = .red
+        
+        // Make dotView fill the controller's view with Auto Layout
+        dotView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(dotView)
+        NSLayoutConstraint.activate([
+            dotView.topAnchor.constraint(equalTo: view.topAnchor),
+            dotView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            dotView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            dotView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: dotView)
+        dotView.configureDot(location: location)
+    }
 }
 
