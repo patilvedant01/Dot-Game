e5acd10b31e4cf966d624904ffba84d1953b9e38
patilvedant01 <vedantpatil324@gmail.com>
2026-01-21 15:15:58 +0530

    Initial Commit

---
+++ AirVeda UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirVeda UIKit
+//
+//  Created by Vedant Patil on 21/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
